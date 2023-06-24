3cdb8b706d97bdcc26bec960eda8b38083f1bad2
mrKuncay008 <kuncaymuhammad@gmail.com>
2023-06-24 12:57:08 +0700

    Edit file loop2.py

--- Perulangan/loop2.py
+++ Perulangan/loop2.py
@@ -1,6 +1,15 @@
 import time
+import os
 
-for misipi in range(6):
-    print("Mississipi")
+# for misipi in range(5):
+#     print("Mississipi")
+#     time.sleep(1)
+# print(misipi)
+os.system("cls")
+isma = int(input("Masukan lope: "))
+
+for i in range(isma):
+    print("Love U :)")
     time.sleep(1)
-print(misipi)
+
+print("Aisma Meo")
