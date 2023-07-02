c316db4ddeff0306c925e4c8524e42005d398542
mrKuncay008 <kuncaymuhammad@gmail.com>
2023-07-03 01:03:13 +0700

    Update

--- Tkinter/kasir.py
+++ Tkinter/kasir.py
@@ -5,33 +5,38 @@ app = Tk()
 app.title('Aplikasi Toko Buku Pren')
 
 # variable
-ap = StringVar()
-s = StringVar()
-k = StringVar()
-db = StringVar()
-t_total = StringVar()
-t_uang = StringVar()
+ap = IntVar()
+s = IntVar()
+k = IntVar()
+db = IntVar()
+t_total = IntVar()
+t_uang = IntVar()
+subtotal = IntVar()
+ppn = IntVar()
 total = 0
 
 # funtion
 
 def totalbeli():
-	global ap, s, k, db, t_total, t_uang
-	ha = int(ap.get()) * 189000
-	hs = int(s.get()) * 165000
-	hk = int(k.get()) * 123000
-	hd = int(db.get()) * 119000
-	global total
-	total = ha + hs + hk + hd
-	t_total.set(locale.format_string("%d", total, grouping=True))
+	global ap, s, k, db, t_total, t_uang, total
+	ha = ap.get() * 189000
+	hs = s.get() * 165000
+	hk = k.get() * 123000
+	hd = db.get() * 119000
+
+	subtotal = ha + hs + hk + hd
+	ppn = subtotal * 0.11
+	total = subtotal + ppn
+	t_total.set(total)
+	
 
 def kembali():
 	global total
-	uang = int(t_uang.get())
+	uang = t_uang.get()
 
 	if uang >= total:
 		kembali = uang - total
-		messagebox.showinfo(message='kembalian anda sebesar{}'.format(kembali))
+		messagebox.showinfo(message='KEMBALIAN Rp.{}'.format(kembali))
 
 def clear():
 	ap.set('0')
@@ -54,7 +59,6 @@ Label(app, text='KASIR TOKO BUKU PREN',
 
 # 1 buku algoritma
 Label(app, text = '1) Algoritma Dan Pemograman', bg='white', foreground='#3D3D3D',font='arial 12 bold').place(x=100,y=100)
-app.grid(column=1, row=0)
 # 2 Buku Struktur data
 Label(app, text = '2) Struktur data', bg='white', foreground='#3D3D3D',font='arial 12 bold').place(x=100,y=140)
 # 3 Buku Keamanan sistem
@@ -79,6 +83,17 @@ Spinbox(app, from_=0,to=100,width=4,font='arial 10',textvariable=s,command=total
 Spinbox(app, from_=0,to=100,width=4,font='arial 10',textvariable=k,command=totalbeli).place(x=550, y=180)
 
 Spinbox(app, from_=0,to=100,width=4,font='arial 10',textvariable=db,command=totalbeli).place(x=550, y=220)
+# Jumlah Total Harga
+Label(app, text='Jumlah Total: ', bg='black',fg='white',font='arial 13 bold italic').place(x=450,y=260)
+
+Label(app, text='Rp.', font='arial 13 bold').place(x=450,y=290)
+
+Label(app, textvariable=t_total, font='arial 13 bold').place(x=475,y=290)
+
+# PPN
+
+Label(app, text='PPN (11 %) ').place(x=450, y=350)
+# Label(app, text='Rp', textvariable=subtotal, fg='black', font='arial 13').place(x=450,y=400)
 
 # Laber Pembayaran
 Label(app,text='Masukan Uang Anda',foreground='black',font='arial 12 bold').place(x= 210,y=260)
@@ -90,5 +105,8 @@ Entry(app,textvariable=t_uang).place(x= 200, y = 300)
 Label(app,text='Rp. ',foreground='black',font='arial 12 bold').place(x=165, y=300)
 
 # Button
-Button(app,text='Total',bg='white',foreground='black',font='arial 12 bold',width=5, command=kembali).place(x=165, y=330)
+Button(app,text='Enter',bg='white',foreground='black',font='arial 12 bold',width=5, command=kembali).place(x=165, y=330)
+
+# Footer text
+Label(app, text='Created By Muhammad Frendi Arahman 4112210', foreground='dark red' ,font='arial 15 bold').place(x=190, y=490)
 app.mainloop()
