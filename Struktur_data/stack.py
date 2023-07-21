e6fd5a309ef68a60e9e3b114a9e1a9078ae02213
tejho <tejho@localhost.localdomain>
2023-07-21 23:13:59 +0700

    Update 21 juli

--- Struktur_data/stack.py
+++ Struktur_data/stack.py
@@ -9,3 +9,15 @@ x = input("Apa Ada kucing yang sudah tiada ?")
 if x in kucing:
     kucing.remove(x)
 print(kucing)
+
+baju = ["itam","Merah","Hijau"]
+print("")
+
+print("Lemari Baju Ku !!!\n")
+print(baju)
+
+nambah = ["Biru","Orange","Kuning"]
+baju.append(nambah)
+print(baju)
+baju.pop()
+print(baju)
